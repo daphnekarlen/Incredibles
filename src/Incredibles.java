f0e095f0fd6c3bfe9de032bf5d35b6f4522dc86c
daphnekarlen <daphne_karlen24@milton.edu>
2022-11-07 12:14:10 -0500

    added bounce and wrap

--- src/Incredibles.java
+++ src/Incredibles.java
@@ -1,4 +1,4 @@
-public class Incredibles {
+public class Incredibles { // comment
 
     //VARIABLE DECLARATION SECTION
     //Here's where you state which variables you are going to use.
@@ -15,10 +15,10 @@ public class Incredibles {
         name = pName;
         xpos = pXpos;
         ypos = pYpos;
-        dx = 1;
-        dy = 1;
-        width = 50;
-        height = 50;
+        dx = -5;
+        dy = 5;
+        width = 100;
+        height = 100;
         isAlive = true;
     }
 
@@ -28,33 +28,39 @@ public class Incredibles {
     }
 
     public void bounce(){
+
         xpos = xpos + dx;
         ypos = ypos + dy;
         // if/when alien hits the right side, reverse dx direction
-        if(xpos==1000-width){
+        if(xpos>=1000-width||xpos<=0){
             dx = -dx;
         }
-        if(ypos==700-width){
+        if(ypos>=700-width||ypos<=0){
             dy=-dy;
         }
+
     }
 
-    public void wrap(){
+    public void wrap() {
         xpos = xpos + dx;
         ypos = ypos + dy;
-
-        if(ypos==800){
-            ypos=0;
+        if (xpos >= 1000 && dx>0) { // right wall
+            xpos = 0-width;
         }
+        if(xpos<=0-width && dx<0){ // left wall
+            xpos=1000;
+        }
+
+        if(ypos<=0-height && dy<0) { // top wall
+            ypos = 700;
+        }
+
+        if(ypos>=700 && dy>0){ // bottom wall
+            ypos=-height;
+        }
+
+
     }
-
-
-
-
-
-
-
-
 
 
 }
